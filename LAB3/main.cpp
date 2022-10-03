a0a247b26466485073986439acc1430aec8507c9
SageCT <egas6554@gmail.com>
2022-10-03 12:40:41 -0500

    Added input code for input.txt files

--- LAB3/main.cpp
+++ LAB3/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <stack>
 #include <algorithm>
 #include "ArgumentManager.h"
 
@@ -9,4 +10,24 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-}
+    // ArgumentManager am(argc, argv);
+    // ifstream input(am.get("input"));
+    // ofstream out(am.get("output"));
+
+    ifstream input("input1.txt");
+    ofstream out("output1.txt");
+    if (input.peek() != EOF)
+    {
+        vector<string> lines;
+        string in;
+
+        while (getline(input, in))
+        {
+            in.erase(remove(in.begin(), in.end(), '\n'), in.end());
+            in.erase(remove(in.begin(), in.end(), '\r'), in.end());
+            in.erase(remove(in.begin(), in.end(), ' '), in.end());
+            if (in.size() == 0)
+                continue;
+            lines.push_back(in);
+        }
+    }
