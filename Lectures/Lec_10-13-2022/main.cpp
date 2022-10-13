d2d01dcf61eb03750359e0b8361054f0d432665c
SageCT <egas6554@gmail.com>
2022-10-13 11:32:01 -0500

    Added Lec 10-13-2022

--- Lectures/Lec_10-13-2022/main.cpp
+++ Lectures/Lec_10-13-2022/main.cpp
@@ -1,6 +1,10 @@
 // For bucketsort, we need to find the digit of the number
 //  write a function to find the length of a number
 
+// Extra 10_2_1 queue as an array implementation
+// Extra 10_2_2 queue as an array with template implementation
+// Extra 10_2_3 queue as a linkedlist implementation
+
 int main()
 {
     return 0;
