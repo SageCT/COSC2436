0a51e576aab77271e3dcade96ec12e267743a083
SageCT <egas6554@gmail.com>
2022-10-22 20:39:25 -0500

    Added mergeSort and merge functions to REVIEW

--- REVIEW/Test 2/TAStuff/TAsorts.cpp
+++ REVIEW/Test 2/TAStuff/TAsorts.cpp
@@ -1,183 +1,179 @@
-//This program contains the code for heap sort, quick sort, merge sort, and shell sort.
-#include<iostream>
+// This program contains the code for heap sort, quick sort, merge sort, and
+// shell sort.
+#include <iostream>
 using namespace std;
 
-void print(int arr[], int size){
-    for(int i = 0; i < size; i++){
-        cout << arr[i] << " ";
-    }
-    cout << endl;
+void print(int arr[], int size) {
+  for (int i = 0; i < size; i++) {
+    cout << arr[i] << " ";
+  }
+  cout << endl;
 }
 
-void heapify(int arr[], int n, int i){
-    int largest = i;
-    int l = 2 * i + 1;
-    int r = 2 * i + 2;
+void heapify(int arr[], int n, int i) {
+  int largest = i;
+  int l = 2 * i + 1;
+  int r = 2 * i + 2;
 
-    if(l < n && arr[l] > arr[largest]){
-        largest = l;
-    }
+  if (l < n && arr[l] > arr[largest]) {
+    largest = l;
+  }
 
-    if(r < n && arr[r] > arr[largest]){
-        largest = r;
-    }
+  if (r < n && arr[r] > arr[largest]) {
+    largest = r;
+  }
 
-    if(largest != i){
-        int temp = arr[largest];
-        arr[largest] = arr[i];
-        arr[i] = temp;
+  if (largest != i) {
+    int temp = arr[largest];
+    arr[largest] = arr[i];
+    arr[i] = temp;
 
-        heapify(arr, n, largest);
-    }
+    heapify(arr, n, largest);
+  }
 }
 
-void heapSort(int arr[], int n){
-    for(int i = n / 2 - 1; i >= 0; i--){
-        heapify(arr, n, i);
-    }
+void heapSort(int arr[], int n) {
+  for (int i = n / 2 - 1; i >= 0; i--) {
+    heapify(arr, n, i);
+  }
 
-    for(int i = n - 1; i > 0; i--){
-        int temp = arr[i];
-        arr[i] = arr[0];
-        arr[0] = temp;
+  for (int i = n - 1; i > 0; i--) {
+    int temp = arr[i];
+    arr[i] = arr[0];
+    arr[0] = temp;
 
-        heapify(arr, i, 0);
-    }
+    heapify(arr, i, 0);
+  }
 }
 
-void merge(int arr[], int i, int j, int k){
-    int mergeSize = k - i + 1;
-    int mergePos = 0;
-    int l = i;
-    int r = j+1;
-    int *nArr = new int[mergeSize];
-    while(l <= j && r <= k){
-        if(arr[l] < arr[r]){
-            nArr[mergePos] = arr[l];
-            l++;
-        }
-        else{
-            nArr[mergePos] = arr[r];
-            r++;
-        }
-        mergePos++;
+void merge(int arr[], int i, int j, int k) {
+  int mergeSize = k - i + 1;
+  int mergePos = 0;
+  int l = i;
+  int r = j + 1;
+  int *nArr = new int[mergeSize];
+  while (l <= j && r <= k) {
+    if (arr[l] < arr[r]) {
+      nArr[mergePos] = arr[l];
+      l++;
+    } else {
+      nArr[mergePos] = arr[r];
+      r++;
     }
-    while(l <= j){
-        nArr[mergePos] = arr[l];
-        mergePos++;
-        l++;
-    }
-    while(r <= k){
-        nArr[mergePos] = arr[r];
-        mergePos++;
-        r++;
-    }
-    for(mergePos = 0; mergePos < mergeSize; mergePos++){
-        arr[mergePos + i] = nArr[mergePos];
-    }
-    delete [] nArr;
+    mergePos++;
+  }
+  while (l <= j) {
+    nArr[mergePos] = arr[l];
+    mergePos++;
+    l++;
+  }
+  while (r <= k) {
+    nArr[mergePos] = arr[r];
+    mergePos++;
+    r++;
+  }
+  for (mergePos = 0; mergePos < mergeSize; mergePos++) {
+    arr[mergePos + i] = nArr[mergePos];
+  }
+  delete[] nArr;
 }
 
-void mergeSort(int arr[], int i, int k){
-    if(i < k){
-        int j = i + (k - i) / 2;
-        mergeSort(arr, i , j);
-        mergeSort(arr, j + 1, k);
-        merge(arr, i , j, k);
-    }
+void mergeSort(int arr[], int i, int k) {
+  if (i < k) {
+    int j = i + (k - i) / 2;
+    mergeSort(arr, i, j);
+    mergeSort(arr, j + 1, k);
+    merge(arr, i, j, k);
+  }
 }
 
-int partition(int arr[], int i, int k){
-    int midpoint = i + (k - i) / 2;
-    int pivot = arr[midpoint];
-    int l = i;
-    int h = k;
-    bool done = false;
-    while(!done){
-        while(arr[l] < pivot){
-            l++;
-        }
-        while(pivot < arr[h]){
-            h--;
-        }
-        if(l >= h){
-            done = true;
-        }
-        else{
-            int temp = arr[l];
-            arr[l] = arr[h];
-            arr[h] = temp;
-            l++;
-            h--;
-        }
+int partition(int arr[], int i, int k) {
+  int midpoint = i + (k - i) / 2;
+  int pivot = arr[midpoint];
+  int l = i;
+  int h = k;
+  bool done = false;
+  while (!done) {
+    while (arr[l] < pivot) {
+      l++;
     }
-    return h;
+    while (pivot < arr[h]) {
+      h--;
+    }
+    if (l >= h) {
+      done = true;
+    } else {
+      int temp = arr[l];
+      arr[l] = arr[h];
+      arr[h] = temp;
+      l++;
+      h--;
+    }
+  }
+  return h;
 }
-void quickSort(int arr[], int i, int k){
-    if(i < k){
-        int j = partition(arr, i, k);
-        quickSort(arr, i, j);
-        quickSort(arr, j+1, k);
-    }
-}
-
-void shellSort(int arr[], int size){
-    for(int gap = size / 2; gap > 0; gap /= 2){
-        for(int i = gap; i < size; i += 1){
-            int temp = arr[i];
-            int j;
-            for(j = i; j >= gap && arr[j-gap] > temp; j -= gap)
-                arr[j] = arr[j-gap];
-            arr[j] = temp;
-        }
-    }
+void quickSort(int arr[], int i, int k) {
+  if (i < k) {
+    int j = partition(arr, i, k);
+    quickSort(arr, i, j);
+    quickSort(arr, j + 1, k);
+  }
 }
 
-int main(){
-
-    int size = 0;
-    cout << "Enter Size of Array: ";
-    cin >> size;
-    int *arr = new int[size];
-
-    int number = 0;
-    cout << "Enter " << size << " numbers to put into the array." << endl;
-    for(int i = 0; i < size; i++){
-        cin >> number;
-        arr[i] = number;
+void shellSort(int arr[], int size) {
+  for (int gap = size / 2; gap > 0; gap /= 2) {
+    for (int i = gap; i < size; i += 1) {
+      int temp = arr[i];
+      int j;
+      for (j = i; j >= gap && arr[j - gap] > temp; j -= gap)
+        arr[j] = arr[j - gap];
+      arr[j] = temp;
     }
+  }
+}
 
-    cout << "Menu:\n1) Heap Sort\n2) Quick Sort\n3) Merge Sort\n4) Shell Sort\n5) Exit" << endl;
-    int choice = 0;
+int main() {
+  int size = 0;
+  cout << "Enter Size of Array: ";
+  cin >> size;
+  int *arr = new int[size];
+
+  int number = 0;
+  cout << "Enter " << size << " numbers to put into the array." << endl;
+  for (int i = 0; i < size; i++) {
+    cin >> number;
+    arr[i] = number;
+  }
+
+  cout << "Menu:\n1) Heap Sort\n2) Quick Sort\n3) Merge Sort\n4) Shell "
+          "Sort\n5) Exit"
+       << endl;
+  int choice = 0;
+  cin >> choice;
+  while (choice != 5) {
+    if (choice == 1) {
+      heapSort(arr, size);
+      cout << "Array sorted using heap sort." << endl;
+      print(arr, size);
+    } else if (choice == 2) {
+      cout << "Array sorted using quick sort." << endl;
+      quickSort(arr, 0, size - 1);
+      print(arr, size);
+    } else if (choice == 3) {
+      cout << "Array sorted using merge sort." << endl;
+      mergeSort(arr, 0, size);
+      print(arr, size);
+    } else if (choice == 4) {
+      cout << "Array sorted using shell sort" << endl;
+      shellSort(arr, size);
+      print(arr, size);
+    } else {
+      cout << "Invalid Choice" << endl;
+    }
     cin >> choice;
-    while(choice != 5){
-        if(choice == 1){
-            heapSort(arr, size);
-            cout << "Array sorted using heap sort." << endl;
-            print(arr, size);
-        }
-        else if(choice == 2){
-            cout << "Array sorted using quick sort." << endl;
-            quickSort(arr, 0, size - 1);
-            print(arr, size);
-        }
-        else if(choice == 3){
-            cout << "Array sorted using merge sort." << endl;
-            mergeSort(arr, 0, size);
-            print(arr, size);
-        }
-        else if(choice == 4){
-            cout << "Array sorted using shell sort" << endl;
-            shellSort(arr, size);
-            print(arr, size);
-        }
-        else{
-            cout << "Invalid Choice" << endl;
-        }
-        cin >> choice;
-    }
+  }
 
-    delete [] arr;
+  delete[] arr;
 
-    return 0;
+  return 0;
 }
--- REVIEW/Test 2/sorts/sorts.cpp
+++ REVIEW/Test 2/sorts/sorts.cpp
@@ -1,20 +1,79 @@
 #include <iostream>
 #include <random>
-
 using namespace std;
 
 void print(int arr[], int size) {
   for (int i = 0; i < size; i++) cout << arr[i] << " ";
-
   cout << endl;
 }
 
-void merge() {}
+void merge(int arr[], int low, int mid, int high) {
+  int l = low;       // Starting index for left subarray
+  int r = mid + 1;   // Starting index for temporary array
+  int mergePos = 0;  // Starting index for right subarray
 
-void mergeSort(int arr[], int a, int b) { if () }
+  int arrSize = (high - low) + 1;
+  int *temp = new int[arrSize];
+
+  while (l <= mid && r <= high) {
+    // arr[i] is smaller than arr[j]
+    if (arr[l] < arr[r]) {
+      temp[mergePos] = arr[l];
+      l++;
+    } else {
+      temp[mergePos] = arr[r];  // arr[j] is smaller than arr[i]
+      r++;
+    }
+
+    mergePos++;
+  }
+
+  // Copying all elements from the left subarray to temp as it is
+  while (l <= mid) {
+    temp[mergePos] = arr[l];
+    mergePos++, l++;
+  }
+  // Copying all elements from the right subarray to temp as it is
+  while (r <= high) {
+    temp[mergePos] = arr[r];
+    mergePos++, r++;
+  }
+
+  for (int x = low; x < arrSize; x++) 
+    arr[x] = temp[x];
+    
+  
+  delete[] temp;
+}
+
+void mergeSort(int *arr, int low, int high) {
+  if (low < high) {
+    int mid = low + (high - low) / 2;
+    mergeSort(arr, low, mid);
+    mergeSort(arr, mid + 1, high);
+
+    merge(arr, low, mid, high);
+  }
+}
 
 void quickSort() {}
 
 void shellSort() {}
 
-int main() { return 0; }
+int main() {
+  int arr[10];
+  for (int x = 0; x < 10; x++) {
+    arr[x] = rand() % 100;
+    cout << arr[x] << " ";
+  }
+  cout << endl;
+
+  mergeSort(arr, 0, 9);
+
+  for (int x = 0; x < 10; x++) {
+    cout << arr[x] << " ";
+  }
+  cout << endl;
+
+  return 0;
+}
