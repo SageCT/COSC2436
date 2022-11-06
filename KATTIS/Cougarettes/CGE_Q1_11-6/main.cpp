167467cf4ce79cb43812f7944c089b3746d54f40
SageCT <egas6554@gmail.com>
2022-11-06 12:10:14 -0600

    Added answers for CGE Kattis 10/6

--- KATTIS/Cougarettes/CGE_Q1_11-6/main.cpp
+++ KATTIS/Cougarettes/CGE_Q1_11-6/main.cpp
@@ -3,4 +3,14 @@
 #include <string>
 #include <vector>
 
-int main() { return 0; }
+using namespace std;
+
+int jack3(int a, int b, int c) { return a * b * c; }
+
+int main() {
+  int a, b, c;
+  cin >> a >> b >> c;
+  cout << jack3(a, b, c);
+
+  return 0;
+}
