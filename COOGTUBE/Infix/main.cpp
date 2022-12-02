e15e8425d1dcd8ff568a87914b7e2fcc0695d490
SageCT <egas6554@gmail.com>
2022-12-02 01:29:53 -0600

    Added coogtube stuff

--- COOGTUBE/Infix/main.cpp
+++ COOGTUBE/Infix/main.cpp
@@ -8,66 +8,6 @@
 
 using namespace std;
 
-void levelOrder1(int *heap, int size) {
-  queue<int> q;
-  // printing the root
-  q.push(heap[0]);
-  string st = to_string(heap[0]);
-  cout << st << endl;
-  q.pop();
-  // Printing children
-  int i = 1;
-  for (int i = 1; i < size; i++) {
-    q.push(heap[i * 2 + 1]);
-    q.push(heap[i * 2 + 2]);
-
-    q.pop();
-    q.pop();
-  }
-}
-
-void levelOrder(int heap[], int size) {
-  queue<int> q;
-  for (int i = 0; i < size; i++) {
-    q.push(heap[i]);
-  }
-
-  int level = 1;
-  int counter = 0;
-  for (int i = 0; i < size; i++) {
-    if (counter == level) {
-      cout << endl;
-      level *= 2;
-      counter = 0;
-    }
-    cout << q.front() << " ";
-    q.pop();
-    counter++;
-  }
-}
-
-void levelOrder3(int heap[], int size) {
-  queue<int> q;
-  for (int i = 0; i < size; i++) {
-    q.push(heap[i]);
-  }
-  int j = 0;
-
-  while (size > 0) {
-    for (int i = 0; i <= j; i++) {
-      int x = q.front();
-      cout << x << " ";
-      q.pop();
-      size--;
-    }
-    j++;
-    cout << endl;
-  }
-}
-
-int partition(int *arr, int low, int high) {}
-
-// PRIORITIES: * and / then + and - then digits.
 int priority(char c) {
   if (c == '/' || c == '*')
     return 2;
@@ -163,37 +103,11 @@ string postfixToInfix(string s) {
   return st.top();
 }
 
-bool validParentheses(string s) {
-  stack<char> st;
-  for (int i = 0; i < s.length(); i++) {
-    if (s[i] == '(' || s[i] == '[' || s[i] == '{') {
-      st.push(s[i]);
-      continue;
-    }
-    char temp;
-    switch (s[i]) {
-      case ')':
-        temp = st.top();
-        st.pop();
-        if (temp != '(') return false;
-        break;
-      case '}':
-        temp = st.top();
-        st.pop();
-        if (temp == '(' || temp == '[') return false;
-        break;
-      case ']':
-        temp = st.top();
-        st.pop();
-        if (temp == '(' || temp == '{') return false;
-        break;
-    }
-  }
-  return (st.empty());
-}
-
 int main() {
-  int heap[15] = {1, 3, 5, 4, 7, 6, 7, 5, 5, 10, 10, 10, 10, 5, 5};
-  int size = 15;
-  levelOrder3(heap, size);
+  string s;
+  cin >> s;
+  cout << endl;
+  cout << "Infix: " << postfixToInfix(s) << endl;
+  cout << "Postfix: " << infixToPostfix(s) << endl;
+  return 0;
 }
--- Lectures/Lec_10-6-2022/Infix to PostFix/main.cpp
+++ Lectures/Lec_10-6-2022/Infix to PostFix/main.cpp
@@ -5,89 +5,82 @@
 
 // Look up Infix to Postfix Expression
 // Look up evaluate postfix expression
-// If you can do infix to postfix send an email to Rizk by Monday for extra points
+// If you can do infix to postfix send an email to Rizk by Monday for extra
+// points
 
 // Extras - Write a program to verify if the expression is balanced
 
 // Below is Infix to Postfix Expression
+#include <algorithm>
 #include <fstream>
+#include <iomanip>
 #include <iostream>
+#include <stack>
 #include <string>
 #include <vector>
-#include <stack>
-#include <algorithm>
-#include <iomanip>
+
 
 using namespace std;
 
-int priority(char l)
-{
-    if (l == '*' || l == '/')
-        return 2;
-    else if (l == '+' || l == '-')
-        return 1;
-    else
-        return -1;
+int priority(char l) {
+  if (l == '*' || l == '/')
+    return 2;
+  else if (l == '+' || l == '-')
+    return 1;
+  else
+    return -1;
 }
 
-string toPostFix(string str)
-{
-    stack<char> s;
-    string result = "";
+string toPostFix(string str) {
+  stack<char> s;
+  string result = "";
 
-    // EXAMPLE INPUT
-    // ( 2 + 4 ) * x = 24
+  // EXAMPLE INPUT
+  // ( 2 + 4 ) * x = 24
 
-    for (int x = 0; x < str.length(); x++)
-    {
-        if (isdigit(str.at(x)) || str.at(x) == 'x')
-            result += str.at(x);
-        else if (str.at(x) == '(')
-            s.push('(');
-        else if (str.at(x) == ')')
-        {
-            while (s.top() != '(')
-            {
-                result += s.top();
-                s.pop();
-            }
-            s.pop();
-        }
-        else
-        {
-            while (!s.empty() && priority(str.at(x)) <= priority(s.top()))
-            {
-                result += s.top();
-                s.pop();
-            }
-            s.push(str.at(x));
-        }
-    }
-    while (!s.empty())
-    {
+  for (int x = 0; x < str.length(); x++) {
+    if (isdigit(str.at(x)) || str.at(x) == 'x')
+      result += str.at(x);
+    else if (str.at(x) == '(')
+      s.push('(');
+    else if (str.at(x) == ')') {
+      while (s.top() != '(') {
         result += s.top();
         s.pop();
+      }
+      s.pop();
+    } else {
+      while (!s.empty() && priority(str.at(x)) <= priority(s.top())) {
+        result += s.top();
+        s.pop();
+      }
+      s.push(str.at(x));
     }
-    return result;
+  }
+  while (!s.empty()) {
+    result += s.top();
+    s.pop();
+  }
+  return result;
 }
 
-int main()
-{
-    string in;
-    vector<string> lines;
-    stack<char> chars;
-    // EXAMPLE INPUT
-    // ( 2 + 4 ) * x = 24
+int main() {
+  string in;
+  vector<string> lines;
+  stack<char> chars;
+  // EXAMPLE INPUT
+  // ( 2 + 4 ) * x = 24
 
-    lines.push_back("(2+4)*x=24");
+  lines.push_back("(2+4)*x=24");
 
-    for (int x = 0; x < lines.size(); x++)
-    {
-        string line = lines.at(x).substr(0, lines.at(x).find("="));
-        double rightSide = stoi(lines.at(x).substr(lines.at(x).find("=") + 1, (lines.at(x).find("=")) - (lines.at(x).length() - 1)));
+  for (int x = 0; x < lines.size(); x++) {
+    string line = lines.at(x).substr(0, lines.at(x).find("="));
+    double rightSide = stoi(lines.at(x).substr(
+        lines.at(x).find("=") + 1,
+        (lines.at(x).find("=")) - (lines.at(x).length() - 1)));
 
-        double ans = 0;
-        string postFix = toPostFix(line);
-        cout << postFix << endl;
-    }
+    double ans = 0;
+    string postFix = toPostFix(line);
+    cout << postFix << endl;
+  }
 }
